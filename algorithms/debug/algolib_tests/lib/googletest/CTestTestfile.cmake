d788a98207f363dd0840697d4c4c335ba4650208
Andrii Yeremenko <andrii.yeremenko@gmail.com>
2023-09-27 10:53:24 +0200

    gitignore fixed

--- algorithms/debug/algolib_tests/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Andrey/Documents/GitHub/learning-algorithms/algorithms/algolib_tests/lib/googletest
-# Build directory: C:/Users/Andrey/Documents/GitHub/learning-algorithms/algorithms/debug/algolib_tests/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
